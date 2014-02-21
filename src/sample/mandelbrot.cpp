449db42ae7aa431747aa90375cb267d744e20a94
psi <ledyba@users.sourceforge.jp>
2014-02-21 14:59:42 +0900

    HSVオーラ

--- src/sample/mandelbrot.cpp
+++ src/sample/mandelbrot.cpp
@@ -10,32 +10,76 @@
 #include <cstdio>
 #include <cmath>
 
-void setPix(cairo_t* cr, const int x, const int y, float a, float r, float g, float b)
+void setSourceHSV(cairo_t* cr, const double h, const double s, const double v)
 {
-	cairo_set_source_rgba(cr, r, g, b, a);
+	const double hh = std::min(h, 360.0);
+	const int i = static_cast<int>(hh / 60.0);
+	const double left = hh - i;
+	const double p = v * (1.0 - s);
+	const double q = v * (1.0 - (s * left));
+	const double t = v * (1.0 - (s * (1.0 - left)));
+
+	double r, g, b;
+
+	switch (i) {
+	case 0:
+		r = v;
+		g = t;
+		b = p;
+		break;
+	case 1:
+		r = q;
+		g = v;
+		b = p;
+		break;
+	case 2:
+		r = p;
+		g = v;
+		b = t;
+		break;
+
+	case 3:
+		r = p;
+		g = q;
+		b = v;
+		break;
+	case 4:
+		r = t;
+		g = p;
+		b = v;
+		break;
+	case 5:
+	default:
+		r = v;
+		g = p;
+		b = q;
+		break;
+	}
+	cairo_set_source_rgba(cr, r, g, b, 1);
+}
+
+void setPix(cairo_t* cr, const int x, const int y, const double f, const int num, const int numMax)
+{
+	//const double sig = 1/(1+pow(M_E, -(f-50)/50));
+	const double sig = (double)num/numMax;
+	setSourceHSV(cr, (sig*180),1,sig);
 	cairo_rectangle(cr, x,y, 1, 1);
 	cairo_fill(cr);
 }
 
 void plot(cairo_t* cr, const int max, const int x, const int y, const double px, const double py)
 {
-	double x_ = 0;
-	double y_ = 0;
-	double x__ = 0;
-	double y__ = 0;
+	double x_=0,y_=0,x__=0,y__=0;
 	for(int i=0;i<max;++i){
 		y_ = 2*x_*y_ + py;
-		if(y_ >= 4){
-			return;
-		}
 		x_ = x__ - y__ + px;
-		if(x_ >= 4){
-			return;
-		}
 		x__ = x_ * x_;
 		y__ = y_ * y_;
+		if(x__+y__ >= 16){
+			setPix(cr, x,y, x__+y__, i, max);
+			return;
+		}
 	}
-	setPix(cr, x,y, 1,0,0,0);
 }
 
 void renderOne(cairo_t* cr, const int max, const int twidth, const int theight, double centerX, double centerY, double width, double height)
@@ -44,7 +88,7 @@ void renderOne(cairo_t* cr, const int max, const int twidth, const int theight, 
 	const int thheight = theight/2;
 	const float scaleX = width / twidth;
 	const float scaleY = height / theight;
-	cairo_set_source_rgba(cr, 1, 1, 1 ,1);
+	cairo_set_source_rgba(cr, 0, 0, 0, 1);
 	cairo_paint(cr);
 	for(int x=0;x<twidth; ++x){
 		for(int y=0;y<theight;++y){
@@ -70,8 +114,8 @@ int main(int argc, char** argv)
 			cairo_paint(r->cairo());
 			const double m = (pow(1.015, i));
 			const double s = 1/m;
-			const double cx = -1.401292+s;
-			renderOne(r->cairo(), i+400, 640, 480, cx, 0, 2*s, 2*s*3/4);
+			const double cx = -1.40115575 +s;
+			renderOne(r->cairo(), i*i/100+300, 640, 480, cx, 0, 2*s, 2*s*3/4);
 			r->shot();
 		}
 	}
